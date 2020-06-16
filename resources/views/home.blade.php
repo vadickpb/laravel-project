e4ab293c58b216e64bf14d376fe8ef2bc1d6859b
Your Name <you@example.com>
2020-06-16 16:58:42 -0500

    maquetacion de imagenes y home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,31 +2,52 @@
 
 @section('content')
 <div class="container">
+    @include('includes.message')
     <div class="row justify-content-center">
-        @include('includes.message')
         
         <div class="col-md-8">
         @foreach ($images as $image)
             <div class="card pub-image">
             
             <div class="card-header">
+                
+                @if ($image->user->image)
+                
+                
                 <div class="container-avatar">
-
-                    @if ($image->user->image)
-
-
-                    <div class="data-user">
+                    
                         <img src=" {{url('user/avatar/'.$image->user->image)}}" alt="" class="avatar">
-                    </div>
-                    @endif
-
-            
                 </div>
-                {{$image->user->name.' '.$image->user->surname }} 
+                
+                @endif
+
+                <div class="data-user">
+                    
+                    {{$image->user->name.' '.$image->user->surname}} 
+
+                <span class="nickname"> {{' | @'.$image->user->nick}}</span>
+                </div>
             
             </div>
 
                 <div class="card-body">
+
+                <div class="image-container">
+
+                    <img src="{{ route('image.file', ['filename' => $image->image_path ]) }}" alt="">
+
+                </div>
+
+                <div class="likes">
+
+                </div>
+
+                <div class="description">
+                    <span class="nickname">{{'@'.$image->user->nick}}</span>
+                    
+                    <p>{{$image->description}}</p> 
+                </div>
+
                      
 
                   
--- routes/web.php
+++ routes/web.php
@@ -55,3 +55,4 @@ Route::post('/user/update','UserController@update')->name('user.update');
 Route::get('/user/avatar/{filename}','UserController@getImage')->name('user.avatar');
 Route::get('/subir-imagen', 'ImageController@create')->name('image.create');
 Route::post('/image/save', 'ImageController@save')->name('image.save');
+Route::get('/image/file/{filename}', 'ImageController@getImage')->name('image.file');
